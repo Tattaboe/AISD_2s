b3cc3236cccca2bf50f4120f45bc4009a0038370
polina <polina@FE60>
2024-04-26 00:31:23 +0400

    Average search time

--- Lab_1/Time.cpp
+++ Lab_1/Time.cpp
@@ -22,7 +22,7 @@ void average_filling_set(size_t size) {
 		result += seconds / 100;
 		//cout << i + 1 << " try: " << seconds << " seconds" << endl;
 	}
-	cout << "SET - Average filling - " << size << " numbers: " << result << endl;
+	cout << "SET - Average filling time - " << size << " numbers: " << result << endl;
 }
 
 void average_filling_vector(size_t size) {
@@ -42,9 +42,75 @@ void average_filling_vector(size_t size) {
 		result += seconds / 100;
 		//cout << i + 1 << " try: " << seconds << " seconds" << endl;
 	}
-	cout << "VECTOR - Average filling -" << size << " numbers: " << result << endl;
+	cout << "VECTOR - Average filling time - " << size << " numbers: " << result << endl;
 }
 
+void average_finding_set(size_t size) {
+	double result = 0.0;
+	Set data;
+	std::mt19937 gen(42);
+	std::uniform_int_distribution<> distrib(-2000000, 2000000);
+	for (size_t i = 0; i < size; i++)
+	{
+		data.insert(distrib(gen));
+	}
+	for (size_t i = 0; i < 1000; i++)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		data.contains(distrib(gen));
+		auto end = std::chrono::high_resolution_clock::now();
+		double seconds = std::chrono::duration<double>(end - start).count();
+		result += seconds / 1000;
+	}
+	cout << "SET - Average finding time - " << size << "numbers: " << result << endl;
+}
+
+void average_finding_vector(size_t size) {
+	double result = 0.0;
+	vector<int> data;
+	std::mt19937 gen(42);
+	std::uniform_int_distribution<> distrib(-2000000, 2000000);
+	for (size_t i = 0; i < size; i++)
+	{
+		data.push_back(distrib(gen));
+	}
+	for (size_t i = 0; i < 1000; i++)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		std::find(data.begin(), data.end(), distrib(gen));
+		auto end = std::chrono::high_resolution_clock::now();
+		double seconds = std::chrono::duration<double>(end - start).count();
+		result += seconds / 1000;
+	}
+	cout << "VECTOR - Average finding time - " << size << " numbers: " << result << endl;
+}
+
+void average_erasing_set(size_t size) {
+	double result = 0.0;
+	std::mt19937 gen(42);
+	std::uniform_int_distribution<> distrib(-2000000, 2000000);
+	Set data;
+	for (size_t i = 0; i < size; i++)
+	{
+		data.insert(distrib(gen));
+	}
+	for (size_t i = 0; i < 1000; i++)
+	{
+		bool deleted = false;
+		int del = distrib(gen);
+		auto start = std::chrono::high_resolution_clock::now();
+		if (data.erase(del)) {
+			deleted = true;
+		}
+		auto end = std::chrono::high_resolution_clock::now();
+		if (deleted) {
+			data.insert(del);
+		}
+		double seconds = std::chrono::duration<double>(end - start).count();
+		result += seconds / 1000;
+	}
+	cout << "SET - Average erasing time - " << size << " numbers: " << result << endl;
+}
 
 int main() {
 
@@ -55,6 +121,19 @@ int main() {
 	average_filling_vector(1000);
 	average_filling_vector(10000);
 	average_filling_vector(100000);
+
+	average_finding_set(1000);
+	average_finding_set(10000);
+	average_finding_set(100000);
+
+	average_finding_vector(1000);
+	average_finding_vector(10000);
+	average_finding_vector(100000);
+
+	average_erasing_set(1000);
+	average_erasing_set(10000);
+	average_erasing_set(100000);
+
 
 	
 }
