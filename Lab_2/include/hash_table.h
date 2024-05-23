e9be486657219666e1ec9a4a9ddb0587d2c9e9f6
polina <polina@FE60>
2024-05-23 17:53:33 +0400

    Start implementing the class and hash function

--- Lab_2/include/hash_table.h
+++ Lab_2/include/hash_table.h
@@ -1,8 +1,71 @@
-﻿// Lab_2.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#ifndef LAB2_INCLUDE_HASHTABLE_H
+#define LAB2_INCLUDE_HASHTABLE_H
 
 #include <iostream>
+#include <vector>
 
-// TODO: Reference additional headers your program requires here.
+
+template <typename K, typename V>
+class HashTable {
+    struct Pair {
+        K key;
+        V value;
+
+    };
+
+    std::vector<Pair> _data;
+    size_t _size;
+
+	size_t hash_func(const K& key) const {
+		if constexpr (std::is_arithmetic_v<K>) {
+			return key % capacity();
+		}
+		else {
+			float hashedSum = 0;
+			for (char character : static_cast<std::string>(key)) {
+				hashedSum += static_cast<float>(character); // ASCII-код символа
+			}
+			return size_t(hashedSum) % capacity();
+		}
+	}
+
+public:
+
+    HashTable(size_t capacity) : _size(0), _data(capacity) {}
+
+    HashTable(const HashTable& other) : _data(other._data), _size(other._size) {}
+
+	HashTable& operator=(const HashTable& other) {
+		if (this == &other)
+			return *this;
+		_data = other._data;
+		_size = other._size;
+	}
+
+	~HashTable() {
+		_data.clear();
+		_size = 0;
+	}
+
+	size_t size() const {
+		return _size;
+	}
+
+	size_t capacity() const {
+		return _data.size();
+	}
+
+	void print() const {
+		for (size_t i = 0; i < _data.size(); ++i) {
+			std::cout << i << ": ";
+			for (const auto& pair : _data[i]) {
+				std::cout << "{" << pair.key << " : " << pair.value << "} ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	
+};
+
+#endif
--- Lab_2/src/main.cpp
+++ Lab_2/src/main.cpp
@@ -1,12 +1,1 @@
-﻿// Lab_2.cpp : Defines the entry point for the application.
-//
-
-#include "Lab_2.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
-}
+﻿#include "../include/hash_table.h"
