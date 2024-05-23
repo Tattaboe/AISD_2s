c5f4c32bbe444cf8438004f50c4070be06c72f0a
polina <polina@FE60>
2024-05-24 01:48:44 +0400

    implementation of insertion using the open addressing method, search functions by key and insertion verification

--- Lab_2/include/hash_table.h
+++ Lab_2/include/hash_table.h
@@ -7,14 +7,19 @@
 
 template <typename K, typename V>
 class HashTable {
-    struct Pair {
-        K key;
-        V value;
+	struct Pair {
+		K key;
+		V value;
 
-    };
+		bool not_empty;
 
-    std::vector<Pair> _data;
-    size_t _size;
+		Pair() : not_empty(true) {}
+		Pair(const K& key, const V& value) : key(key), value(value), not_empty(false) {}
+
+	};
+
+	std::vector<Pair> _data;
+	size_t _size;
 
 	size_t hash_func(const K& key) const {
 		if constexpr (std::is_arithmetic_v<K>) {
@@ -31,9 +36,9 @@ class HashTable {
 
 public:
 
-    HashTable(size_t capacity) : _size(0), _data(capacity) {}
+	HashTable(size_t capacity) : _size(0), _data(capacity) {}
 
-    HashTable(const HashTable& other) : _data(other._data), _size(other._size) {}
+	HashTable(const HashTable& other) : _data(other._data), _size(other._size) {}
 
 	HashTable& operator=(const HashTable& other) {
 		if (this == &other)
@@ -56,16 +61,75 @@ public:
 	}
 
 	void print() const {
-		for (size_t i = 0; i < _data.size(); ++i) {
-			std::cout << i << ": ";
-			for (const auto& pair : _data[i]) {
+		for (const Pair& pair : _data) {
+			if (!pair.not_empty)
 				std::cout << "{" << pair.key << " : " << pair.value << "} ";
-			}
-			std::cout << std::endl;
 		}
+		std::cout << std::endl;
 	}
 
-	
+	bool insert(const K& key, const V& value) {
+		size_t index = hash_func(key) % _data.size();
+		size_t original_index = index;
+		size_t first_empty_index = _data.size();
+
+		while (!_data[index].not_empty) {
+			if (_data[index].key == key) {
+				return false;
+			}
+			if (_data[index].not_empty && first_empty_index == _data.size()) {
+				first_empty_index = index;
+			}
+			index = (index + 1) % _data.size();
+			if (index == original_index) {
+				if (first_empty_index != _data.size()) {
+					_data[first_empty_index] = Pair(key, value);
+					_size++;
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+		_data[index] = Pair(key, value);
+		_size++;
+		return true;
+
+	}
+
+	bool insert_or_assign(const K& key, const V& value) {
+		Pair* find_pair = search(key);
+		if (find_pair) {
+			find_pair->value = value;
+			return false;
+		}
+		insert(key, value);
+	}
+
+	bool contains(const V& value) {
+		for (Pair& pair : _data)
+			if (pair.value == value && !pair.not_empty)
+				return true;
+		return false;
+	}
+
+	Pair* search(const K& key) {
+		size_t start_index = hash_func(key) % _data.size();
+		if ((_data[start_index].key == key) && (!_data[start_index].not_empty)) {
+			return &_data[start_index];
+		}
+		size_t index = (start_index + 1) % _data.size();
+		while (index != start_index) {
+			if ((_data[index].key == key) && (!_data[index].not_empty)) {
+				return &_data[index];
+			}
+			index = (index + 1) % _data.size();
+		}
+		return nullptr;
+	}
+
+
 };
 
 #endif
--- Lab_2/src/main.cpp
+++ Lab_2/src/main.cpp
@@ -1,1 +1,23 @@
 ﻿#include "../include/hash_table.h"
+
+int main() {
+	std::cout << "Insert check" << std::endl;
+	HashTable<int, int> hasht(10);
+
+	hasht.insert(1, 3);
+	hasht.insert(2, 11);
+	hasht.insert(3, -1);
+	hasht.insert(3, 8);
+	hasht.insert(4, 6);
+	hasht.insert(8, -7);
+	hasht.insert(5, 2);
+	hasht.insert(7, 3);
+	hasht.insert(7, 19);
+	hasht.insert(6, 10);
+	
+
+
+	hasht.print();
+	
+	return 0;
+}
